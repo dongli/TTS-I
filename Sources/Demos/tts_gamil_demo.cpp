50398a6d58a1dab35a376ba7465e37bad9c347ba
Li Dong <dongli@lasg.iap.ac.cn>
2012-02-25 13:49:47 +0800

    Add test code

--- Sources/Demos/tts_gamil_demo.cpp
+++ Sources/Demos/tts_gamil_demo.cpp
@@ -20,6 +20,8 @@ int main(int argc, char **argv)
     // -------------------------------------------------------------------------
     gamilReader.init("gamil_data", "tts.gamil.suv.*.nc");
     gamilReader.getVelocityField();
+    // TEST: Test PolygonRezoner
+    TimeManager::setEndStep(72);
     // -------------------------------------------------------------------------
     while (!timeManager.isFinished()) {
         tts.advect(gamilReader.meshManager, gamilReader.meshAdaptor,
